b6a3e0c5f473e3f3bebdf75ecfc162e8a8eb27ce
Allen Wu <65364518+allenwu038@users.noreply.github.com>
2020-05-18 17:16:36 -0700

    Update 674_LongestContinuousIncreasingSubsequence.py

--- 674_LongestContinuousIncreasingSubsequence.py
+++ 674_LongestContinuousIncreasingSubsequence.py
@@ -20,3 +20,25 @@ class Solution:
             if count > maxLength:
                 maxLength = count
         return maxLength
+
+# solution 2
+# runtime: faster than 15.44% of submissions
+# memory: less than 8.70% of submissions
+
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if nums == []:
+            return 0
+        maxLength = 1
+        count = 1
+        previous = nums[0]
+        for current in nums:
+            # compare current number in nums to the one before
+            if previous < current:
+                count +=1
+            else:
+                count = 1
+            if count > maxLength:
+                maxLength = count
+            previous = current
+        return maxLength
